c8b6bcc9ab91ed146ece9e7b1ce902b0a3c9b8e1
abhishek507 <abhishekeluru@gmail.com>
2023-09-19 13:39:52 +0530

    first commit

---
+++ js/app.js
@@ -0,0 +1,19 @@
+function roll(){
+    let audio = document.getElementById('audio');
+    audio.play();
+
+    document.getElementById('diceValue').innerHTML = '';
+    document.getElementById('dice').classList.add('dice-animation')
+
+   setTimeout(()=>{
+    let diceNumber = Math.floor(Math.random()*6);
+    let diceValue = document.getElementById('diceValue');
+    diceValue.innerHTML = diceNumber;
+   },2000) 
+
+
+
+    setTimeout(()=>{
+        document.getElementById('dice').classList.remove('dice-animation');
+    },2000)
+}
